f596857a44a69ca729bbc55ebeebbc3a50c21e83
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-21 15:48:20 +0100

    esercizio 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studente2 = students.find((two)=>{
+  return two.id === 2
+})
+
+  console.log(studente2);
+  
